f6588d5c7f631d50a0926d8ddb1ce16c14fea621
Young-ook Kim <youngook.kim@gmail.com>
2023-11-14 20:09:50 +0900

    feat(tfstate): frigga naming

--- labels.tf
+++ labels.tf
@@ -1,18 +1,15 @@
-resource "random_string" "suffix" {
-  length  = 5
-  upper   = false
-  lower   = true
-  number  = false
-  special = false
+### frigga name
+module "frigga" {
+  source  = "Young-ook/spinnaker/aws//modules/frigga"
+  version = "3.0.0"
+  name    = var.name == null || var.name == "" ? "tfstate" : var.name
+  petname = true
 }
 
 locals {
-  bucket-name = join("-", compact([var.name, "tfstate", random_string.suffix.result]))
-  table-name  = join("-", compact([var.name, "tflock", random_string.suffix.result]))
-
-  bucket-nametag = { "Name" = local.bucket-name }
-  table-nametag  = { "Name" = local.table-name }
+  name = module.frigga.name
   default-tags = merge(
     { "terraform.io" = "managed" },
+    { "Name" = local.name }
   )
 }
