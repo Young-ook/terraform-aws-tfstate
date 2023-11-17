3fb03078a30a9365bcf4825b5cbbe19ebb9cd54f
Young-ook Kim <youngook.kim@gmail.com>
2023-11-17 15:29:38 +0900

    feat(tfstate): generate a backend config file

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,7 @@
-output "backend" {
-  description = "Terraform state backend configuration"
-  value       = <<-EOT
+### outputs
+
+locals {
+  backend_config = <<-EOT
   terraform {
     backend "s3" {
       region = "${module.aws.region.name}"
@@ -9,4 +10,16 @@ output "backend" {
     }
   }
   EOT
+}
+
+output "backend" {
+  description = "Terraform state backend configuration"
+  value       = local.backend_config
+}
+
+### terraform state backend configuration file
+resource "local_file" "backend" {
+  content         = local.backend_config
+  filename        = "${path.cwd}/backend.tf"
+  file_permission = "0600"
 }
